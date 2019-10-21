36b1f131afcbe417fb0bd3f7d85087c2bb78fe26
Phillip Oatmeal <poatmeal@ox-i5.21-school.ru>
2019-10-21 15:21:30 +0300

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poatmeal <poatmeal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/25 14:18:08 by poatmeal          #+#    #+#             */
+/*   Updated: 2019/10/15 16:09:05 by poatmeal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <sys/types.h>
+
+# define BUFF_SIZE 32
+
+int						get_next_line(const int fd, char **line);
+
+#endif
